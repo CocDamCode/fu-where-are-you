c0b6dc1f92000e86de4e31350759f1d71a306bf6
Dinh Quang Trung <trungdq88@gmail.com>
2014-09-14 12:03:27 +0700

    Finish API

--- fuway-back/utils/dal.php
+++ fuway-back/utils/dal.php
@@ -10,7 +10,7 @@ include_once 'db.php';
 function search_person($str)
 {
     $keyword = mysql_real_escape_string("%" . $str . "%");
-    $result = mysql_query("SELECT DISTINCT person_name, person_code, role, email FROM fuway_schedule WHERE person_name LIKE \"$keyword\" OR email LIKE \"$keyword\"") or die(mysql_error($db));
+    $result = execute_query("SELECT DISTINCT person_name, person_code, role, email FROM fuway_schedule WHERE person_name LIKE \"$keyword\" OR email LIKE \"$keyword\"");
 
     $persons = [];
     if ($result) {
@@ -27,20 +27,29 @@ function search_person($str)
 }
 
 function get_person_schedule($str, $date) {
+    if (!$date) return [];
     $email = mysql_real_escape_string($str);
     $query_date = date_format($date, 'Y-m-d');
-    $result = mysql_query("SELECT * FROM fuway_schedule WHERE email = \"$email\" slotdate = \"$query_date\"");
+    $result = execute_query("SELECT * FROM fuway_schedule WHERE email = \"$email\" AND slotdate = \"$query_date\"");
 
-
+    $slots = [];
     if ($result) {
         while ($row = mysql_fetch_array($result)) {
+            $slot = [];
             $person = [];
             $person["Email"] = $row["email"];
             $person["Code"] = $row["person_code"];
             $person["Name"] = $row["person_name"];
             $person["Role"] = $row["role"];
-            array_push($persons, $person);
+
+            $slot["Person"] = $person;
+            $slot["Date"] = strtotime($row["slotdate"]);
+            $slot["Slot"] = (int) $row["slot"];
+            $slot["Room"] = $row["room"];
+            $slot["Class"] = $row["class"];
+            $slot["Course"] = $row["course"];
+            array_push($slots, $slot);
         }
     }
-    // return $slots;
+    return $slots;
 }
