eefb71fdb2b9dec6d76a2c4e9f383edb433f2264
Dinh Quang Trung <trungdq88@gmail.com>
2014-09-14 09:41:19 +0700

    Add snippet

--- fuway-back/index.php
+++ fuway-back/index.php
@@ -1,3 +1,25 @@
 <?php
 
-    var_dump($_GET);
+    if (isset($_GET['search'])) {
+        $persons = array();
+        $person1 = [];
+        $person1["Email"] = "trungdqse60994@fpt.edu.vn";
+        $person1["Code"] = "SE60994";
+        $person1["Name"] = "Đinh Quang Trung";
+
+        $person2 = [];
+        $person2["Email"] = "tuanbtse60824@fpt.edu.vn";
+        $person2["Code"] = "SE60824";
+        $person2["Name"] = "Bùi Tiến Tuân";
+
+        $person3 = [];
+        $person3["Email"] = "hainntse60916@fpt.edu.vn";
+        $person3["Code"] = "SE60916";
+        $person3["Name"] = "Nguyễn Ngọc Thanh Hải";
+
+        array_push($persons, $person1);
+        array_push($persons, $person2);
+        array_push($persons, $person3);
+
+        echo json_encode($persons);
+    }
